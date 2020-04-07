a875ff203bdc6f9ea0cd32ee5fff186d8696b0d5
NGUYEN DUC VIET <ducvietnguyen195@live.com>
2020-04-07 17:28:20 +0700

    Add Identity User and seeding userdata

--- eShopSolution.Data/Migrations/20200407101533_AddAspNetCoreIdentityDataBase.Designer.cs
+++ eShopSolution.Data/Migrations/20200407101533_AddAspNetCoreIdentityDataBase.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eShopSolution.Data.EF;
 
 namespace eShopSolution.Data.Migrations
 {
     [DbContext(typeof(EShopDbContext))]
-    partial class EShopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200407101533_AddAspNetCoreIdentityDataBase")]
+    partial class AddAspNetCoreIdentityDataBase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -92,13 +94,6 @@ namespace eShopSolution.Data.Migrations
                     b.HasKey("UserId", "RoleId");
 
                     b.ToTable("AppUserRoles");
-
-                    b.HasData(
-                        new
-                        {
-                            UserId = new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
-                            RoleId = new Guid("8d04dce2-969a-435d-bba4-df3f325983dc")
-                        });
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityUserToken<System.Guid>", b =>
@@ -175,16 +170,6 @@ namespace eShopSolution.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("AppRoles");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"),
-                            ConcurrencyStamp = "b3ebfd51-d7ca-4f8c-adf3-612d4af7654c",
-                            Description = "Administrator role",
-                            Name = "admin",
-                            NormalizedName = "admin"
-                        });
                 });
 
             modelBuilder.Entity("eShopSolution.Data.Entities.AppUser", b =>
@@ -251,27 +236,6 @@ namespace eShopSolution.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("AppUsers");
-
-                    b.HasData(
-                        new
-                        {
-                            Id = new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"),
-                            AccessFailedCount = 0,
-                            ConcurrencyStamp = "cb8e8e1b-5947-49ee-89f8-fb6126522582",
-                            Dob = new DateTime(2020, 1, 31, 0, 0, 0, 0, DateTimeKind.Unspecified),
-                            Email = "ducvietnguyen195@gmail.com",
-                            EmailConfirmed = true,
-                            FirstName = "Viet",
-                            LastName = "Nguyen",
-                            LockoutEnabled = false,
-                            NormalizedEmail = "ducvietnguyen195@gmail.com",
-                            NormalizedUserName = "admin",
-                            PasswordHash = "AQAAAAEAACcQAAAAEHQ1JnzGptgv3oM2zgEUGfp6co9hNG2bu8uH23yKATcu7G9M7rvoUOaqzX4iYzwvbg==",
-                            PhoneNumberConfirmed = false,
-                            SecurityStamp = "",
-                            TwoFactorEnabled = false,
-                            UserName = "admin"
-                        });
                 });
 
             modelBuilder.Entity("eShopSolution.Data.Entities.Cart", b =>
@@ -521,7 +485,7 @@ namespace eShopSolution.Data.Migrations
                     b.Property<DateTime>("OrderDate")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("datetime2")
-                        .HasDefaultValue(new DateTime(2020, 4, 7, 17, 25, 33, 292, DateTimeKind.Local).AddTicks(5366));
+                        .HasDefaultValue(new DateTime(2020, 4, 7, 17, 15, 31, 636, DateTimeKind.Local).AddTicks(8900));
 
                     b.Property<string>("ShipAddress")
                         .IsRequired()
@@ -617,7 +581,7 @@ namespace eShopSolution.Data.Migrations
                         new
                         {
                             Id = 1,
-                            DateCreated = new DateTime(2020, 4, 7, 17, 25, 33, 367, DateTimeKind.Local).AddTicks(7467),
+                            DateCreated = new DateTime(2020, 4, 7, 17, 15, 31, 688, DateTimeKind.Local).AddTicks(8723),
                             OriginalPrice = 100000m,
                             Price = 200000m,
                             Stock = 0,
---
+++ eShopSolution.Data/Migrations/20200407102535_Seeding_User_Data.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class Seeding_User_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2020, 4, 7, 17, 25, 33, 292, DateTimeKind.Local).AddTicks(5366),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 4, 7, 17, 15, 31, 636, DateTimeKind.Local).AddTicks(8900));
+
+            migrationBuilder.InsertData(
+                table: "AppRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
+                values: new object[] { new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"), "b3ebfd51-d7ca-4f8c-adf3-612d4af7654c", "Administrator role", "admin", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AppUserRoles",
+                columns: new[] { "UserId", "RoleId" },
+                values: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), new Guid("8d04dce2-969a-435d-bba4-df3f325983dc") });
+
+            migrationBuilder.InsertData(
+                table: "AppUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Dob", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), 0, "cb8e8e1b-5947-49ee-89f8-fb6126522582", new DateTime(2020, 1, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), "ducvietnguyen195@gmail.com", true, "Viet", "Nguyen", false, null, "ducvietnguyen195@gmail.com", "admin", "AQAAAAEAACcQAAAAEHQ1JnzGptgv3oM2zgEUGfp6co9hNG2bu8uH23yKATcu7G9M7rvoUOaqzX4iYzwvbg==", null, false, "", false, "admin" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 4, 7, 17, 25, 33, 367, DateTimeKind.Local).AddTicks(7467));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("8d04dce2-969a-435d-bba4-df3f325983dc"));
+
+            migrationBuilder.DeleteData(
+                table: "AppUserRoles",
+                keyColumns: new[] { "UserId", "RoleId" },
+                keyValues: new object[] { new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"), new Guid("8d04dce2-969a-435d-bba4-df3f325983dc") });
+
+            migrationBuilder.DeleteData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("69bd714f-9576-45ba-b5b7-f00649be00de"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 4, 7, 17, 15, 31, 636, DateTimeKind.Local).AddTicks(8900),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 4, 7, 17, 25, 33, 292, DateTimeKind.Local).AddTicks(5366));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 4, 7, 17, 15, 31, 688, DateTimeKind.Local).AddTicks(8723));
+        }
+    }
+}

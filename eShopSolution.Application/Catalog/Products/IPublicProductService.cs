0818fb01782cb9e39671da1a5b667be34391fa06
NGUYEN DUC VIET <ducvietnguyen195@live.com>
2020-04-08 17:04:59 +0700

    Add API Back End

--- eShopSolution.Application/Catalog/Products/IPublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -1,12 +1,14 @@
 ﻿
 using eShopSolution.ViewModel.Catalog.Products;
 using eShopSolution.ViewModel.Common;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace eShopSolution.Application.Catalog.Products
 {
     public interface IPublicProductService
     {
+        Task<List<ProductViewModel>> GetAll();
         Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request);
     }
 }
---
+++ eShopSolution.Utilities/Contansts/SystemConstanst.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Utilities.Contansts
+{
+  public  class SystemConstanst
+    {
+        public const string MainConnectionString = "eShopSolutionDb";
+    }
+}

3130868b1cf93748d3a5202412f11fc3190a3e8a
NGUYEN DUC VIET <ducvietnguyen195@live.com>
2020-04-09 17:38:42 +0700

    Create login and register APIs

---
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.Application.System.Users;
+using eShopSolution.ViewModel.Catalog.System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Authenticate([FromForm]LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tokenResult = await _userService.Authenticate(request);
+
+            if (string.IsNullOrEmpty(tokenResult))
+                return BadRequest("Username or password is incorrect");
+
+            return Ok(new { token = tokenResult });
+        }
+
+        [HttpPost("register")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register([FromForm]RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _userService.Register(request);
+
+            if (!result)
+                return BadRequest("Register is unsuccessful");
+
+            return Ok();
+        }
+    }
+}

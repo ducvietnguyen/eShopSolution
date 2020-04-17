d4b770014ca862719ba2500d1288d07dc0ae60cd
NGUYEN DUC VIET <ducvietnguyen195@live.com>
2020-04-17 11:34:08 +0700

    Add Addmin Application

---
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using eShopSolution.ViewModel.Catalog.System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.AdminApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginRequest request)
+        {
+            return View();
+        }
+    }
+}
